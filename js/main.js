62c0164534bfff337018c1cb797a29b0dd488462
Aabid Ali <154051924+aabidali641@users.noreply.github.com>
2024-07-31 18:29:29 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// header scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 50){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+
+// navbar hide hogi jab ham kisi dusre page par redirect karenge
+
+let navBar = document.querySelectorAll(".nav-links");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(e){
+    e.addEventListener("click" , function(){
+        navCollapse.classList.remove("show");
+    })
+});
